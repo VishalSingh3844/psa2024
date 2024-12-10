3a5873ae11a41ad4c0db7998b22d28ea55012a4a
vishal <vishalsingh3844@gmail.com>
2024-12-10 16:30:06 +0530

    test 1

---
+++ src/main/java/com/crm/entity/Post.java
@@ -0,0 +1,47 @@
+package com.crm.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 4000)
+    private String description;
+
+    @OneToMany(mappedBy = "post",cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
